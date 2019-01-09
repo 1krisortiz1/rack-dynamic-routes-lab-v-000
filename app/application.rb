9578cb027f4d0b93438db204c49369f867afb6ea
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-01-09 15:09:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,10 +8,10 @@ class Application
 
         item_name = req.path.split("/items").last
         item = @@items.find{|i| i.name == item_name
-    elsif
+    else
         req.path=="/testing"
-      resp.write "Route not found"
-      resp.status = 404
+        resp.write "Route not found"
+        resp.status = 404
     end
 
     resp.finish
